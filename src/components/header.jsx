6ba39f1a6a134938e4712b4ed4a7169c39eedc22
yourstejasvi <tejasvis3103@gmail.com>
2024-01-07 20:13:41 +0530

    First commit

---
+++ src/components/header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Logo from "../Fenty Assets/logo.png";
+
+
+const header = () => {
+  return (
+    <>
+        <div className='w-full h-[120px] bg-slate-100 shadow-lg'>
+            <div className='flex flex-row'>
+
+            <img src={Logo} className='h-24 ml-[50px] my-4 drop-shadow-xl'/>
+            <h2 className='font-salsa text-7xl p-4 mt-4 drop-shadow-xl tracking-wider'>Vixen</h2>
+
+            </div>
+
+            <div className='flex flex-row justify-around ml-[750px] -mt-16'>
+                <a className='text-xl font-semibold cursor-pointer hover:shadow-inner'>HOME</a>
+                <a className='text-xl font-semibold cursor-pointer hover:shadow-inner'>PRODUCTS</a>
+                <a className='text-xl font-semibold cursor-pointer hover:shadow-inner'>STORES</a>
+                <a className='text-xl font-semibold cursor-pointer hover:shadow-inner'>CONTACT-US</a>
+
+            </div>
+            
+
+        </div>
+
+    </>
+  )
+}
+
+export default header
