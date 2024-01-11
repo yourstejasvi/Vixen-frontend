f91d3c4e898e6110132f266be6f3f2d9b3ba71b1
yourstejasvi <tejasvis3103@gmail.com>
2024-01-11 22:43:41 +0530

    Improved footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,53 @@ const Footer = () => {
     <>
     <footer className='h-[300px] bg-slate-200 mt-[100px] drop-shadow-[150px_155px_205px_rgba(0,0,0,1)]'>
 
-      <div className='flex flex-col p-[80px] ml-[100px]'>
-        <h1 className='p-1 text-lg tracking-widest font-bold hover:underline'>Careers</h1>
-        <h1 className='p-1 text-lg tracking-widest font-bold hover:underline'>About</h1>
-        <h1 className='p-1 text-lg tracking-widest font-bold hover:underline'>Customer reviews</h1>
-        <h1 className='p-1 text-lg tracking-widest font-bold hover:underline'>File a complaint</h1>
+      <div className='flex flex-col p-[20px] -ml-[1100px] text-center'>
+        <h1 className='font-salsa font-bold tracking-widest text-3xl ml-3'>Vixen</h1>
+        <div className='mt-5'>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Who are we?</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Careers</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Testimonials</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Vixen CSR</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Investor relations</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Authenticity</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Responsible disclosure</h1>
+        </div>
       </div>
 
-      <div className='flex flex-col p-[80px] ml-[1050px] -mt-[300px]'>
-        <h1 className='p-1 text-lg tracking-widest font-bold hover:underline'>© Tejasvi Sharma</h1>
-        <h1 className='p-1 text-lg tracking-widest font-bold hover:underline'>© tejasvis3103@gmail.com</h1>
-        <h1 className='p-1 text-lg tracking-widest font-bold hover:underline'>© Vixen-2024</h1>
-      
+      <div className='flex flex-col p-[20px] -ml-[340px] -mt-[270px] text-center'>
+        <h1 className='font-salsa font-bold tracking-widest text-xl ml-3'>Help</h1>
+        <div className='mt-5'>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Contact Us</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Frequently asked questions</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Cancellation & Return</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Shipping & Return</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Sell on Vixen</h1>
+        </div>
+      </div>
+
+      <div className='flex flex-col p-[20px] ml-[360px] -mt-[232px] text-center'>
+        <h1 className='font-salsa font-bold tracking-widest text-xl ml-3'>Top Categories</h1>
+        <div className='mt-5'>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Makeup</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Skin</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Hair</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Fragrance</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Natural</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>Bath & Body</h1>
+        </div>
+      </div>
+
+      <div className='flex flex-col p-[80px] ml-[1050px] -mt-[322px] text-center'>
+        <h1 className='font-salsa font-bold tracking-widest text-xl ml-3'>Developer</h1>
+
+        <div className='mt-5'>
+          
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>© Tejasvi Sharma</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>© tejasvis3103@gmail.com</h1>
+            <h1 className='p-1 text-md tracking-widest font-bold hover:underline'>© Vixen-2024</h1>
+
+
+        </div>
       </div>
 
     </footer>
